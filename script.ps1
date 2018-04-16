bff66d441de5cba3a5514f04d4a42795a923fefd
Tarik <tarikaboulhouda@gamil.com>
2018-04-16 16:25:46 +0200

    ff

---
+++ script.ps1
@@ -0,0 +1,33 @@
+﻿$var1 = "cha va"
+
+$fruits = @("pomme", "poire", 45, $var1)
+
+$user = @{Nom="Aboulhouda"; Prenom="Tarik"; Age=23; Role="éleve"}
+<#
+foreach($item in $fruits){
+    $item
+    Write-Host "je tourne"
+}
+#>
+<#
+for($i = 0; $i -lt 10; $i++){
+    $fruits[$i]
+}
+#>
+
+<#
+$i = 0
+
+while ($i -lt 11){
+    $i * 5
+    $i++
+}
+#>
+
+function hello($name)
+{
+    "Hello " + [string]$name
+}
+
+hello("Tarik")
+hello("Toto")
